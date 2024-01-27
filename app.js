7291568bb76dfed2491804ac42b1a7d20b1d5d49
jabirmussa <jabirmussa185@gmail.com>
2024-01-27 13:46:24 +0200

    bixa

---
+++ app.js
@@ -0,0 +1,23 @@
+/*pwa*/
+async function registerSW() {
+    if ('serviceworker' in navigator){
+        try{
+            await navigator
+                .serviceWorker
+                .register('serviceworker.js');
+        }
+        catch(e){
+            console.log('SW registation failed');
+        }
+    }
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registrado com sucesso:', registration);
+      })
+      .catch(function(error) {
+        console.error('Falha ao registrar o Service Worker:', error);
+      });
+  }
